9dc13130cf79235b7a0eb9f9bde5e29d6378be6e
Mohamed Ayman <mohikel6@gmail.com>
2022-08-24 05:17:33 +0200

    added files

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=imac
+export POSTGRES_PASSWORD=pass123
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=udagram
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=this_is_jwt
+export URL=http://localhost
